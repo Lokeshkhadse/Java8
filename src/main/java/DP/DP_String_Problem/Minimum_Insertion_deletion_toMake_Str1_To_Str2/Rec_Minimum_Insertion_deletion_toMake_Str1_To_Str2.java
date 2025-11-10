c8f02e079e5206bff519ce41a6233b81ed01edcb
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-11-10 14:22:30 +0530

    added

--- src/main/java/DP/DP_String_Problem/Minimum_Insertion_deletion_toMake_Str1_To_Str2/Rec_Minimum_Insertion_deletion_toMake_Str1_To_Str2.java
+++ src/main/java/DP/DP_String_Problem/Minimum_Insertion_deletion_toMake_Str1_To_Str2/Rec_Minimum_Insertion_deletion_toMake_Str1_To_Str2.java
@@ -8,7 +8,7 @@ public class Rec_Minimum_Insertion_deletion_toMake_Str1_To_Str2 {
         int m = s1.length();
         int n = s2.length();
 
-        int lcs = lcsRecursive(s1, s2, m, n);
+        int lcs = lcsRecursive(s1, s2, m-1, n-1);
         int ans = (m - lcs) + (n - lcs);
         System.out.println("Minimum_Insertion_deletion_toMake_Str1_To_Str2 is ---> " + ans);
     }
@@ -16,19 +16,14 @@ public class Rec_Minimum_Insertion_deletion_toMake_Str1_To_Str2 {
     // Recursive LCS function
     public static int lcsRecursive(String s1, String s2, int m, int n) {
         // base cases
-        if (m == 0 || n == 0)
+        if (m < 0 || n < 0)
             return 0;
 
         // if last characters match
-        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+        if (s1.charAt(m ) == s2.charAt(n)) {
             return 1 + lcsRecursive(s1, s2, m - 1, n - 1);
         }
         // if not match — skip one char either from s1 or s2
-        else {
-            return Math.max(
-                    lcsRecursive(s1, s2, m - 1, n),
-                    lcsRecursive(s1, s2, m, n - 1)
-            );
-        }
+        return 0 + Math.max(lcsRecursive(s1,s2,m-1,n),lcsRecursive(s1,s2,m,n-1));
     }
 }
