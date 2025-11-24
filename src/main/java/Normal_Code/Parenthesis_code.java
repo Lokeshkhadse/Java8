0357f32f8b760177b209584e93c6131da0fc7b51
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-11-24 15:36:48 +0530

    added

---
+++ src/main/java/Normal_Code/Parenthesis_code.java
@@ -0,0 +1,34 @@
+package Normal_Code;
+
+import java.util.Stack;
+
+public class Parenthesis_code {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        boolean ans = isValid(s);
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch:s.toCharArray()){
+            if(ch =='(' || ch =='{' || ch =='['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+
+                char prev = stack.pop();
+
+                if(ch == ')' && prev == '(' || ch == '}' && prev == '{' || ch == ']' && prev == '['){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
