7288ef39ae4f61e014aa349c371df2ee7f9f3509
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2026-02-17 18:06:57 +0530

    added

---
+++ src/main/java/Trees/View_Of_BinaryTree/Right_View.java
@@ -0,0 +1,68 @@
+package Trees.View_Of_BinaryTree;
+import java.util.*;
+
+public class Right_View {
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Pair {
+        int row;
+        TreeNode node;
+
+        Pair(int row, TreeNode node) {
+            this.row = row;
+            this.node = node;
+        }
+    }
+
+    public static List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> list = new LinkedList<>();
+        if (root == null) return list;
+
+        Queue<Pair> q = new LinkedList<>();
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        q.add(new Pair(0, root));
+
+        while (!q.isEmpty()) {
+
+            Pair pair = q.poll();
+            int currRowNo = pair.row;
+            TreeNode temp = pair.node;
+
+            // first node at level (rightmost because we push right first)
+            if (!map.containsKey(currRowNo)) {
+                map.put(currRowNo, temp.val);
+            }
+
+            if (temp.right != null)
+                q.add(new Pair(currRowNo + 1, temp.right));
+
+            if (temp.left != null)
+                q.add(new Pair(currRowNo + 1, temp.left));
+        }
+
+        for (Map.Entry<Integer, Integer> mp : map.entrySet())
+            list.add(mp.getValue());
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.left.left = new TreeNode(5);
+
+        System.out.println(rightSideView(root));
+    }
+}
